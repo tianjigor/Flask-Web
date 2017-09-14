269ff0a9cfe9d71659730a608c4465fb8e3dfe2d
Tianjigor <541878782@qq.com>
2017-09-14 23:07:37 +0800

    8 用户认证

---
+++ app/__init__.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, make_response, abort, render_template, session, redirect, url_for, flash
+from flask_script import Manager
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+from flask_sqlalchemy import SQLAlchemy
+import os
+from flask_script import Manager, Shell
+from flask_migrate import Migrate, MigrateCommand
+from flask_mail import Mail, Message
+from threading import  Thread
+from config import config
+from flask_login import LoginManager
+
+
+bootstrap = Bootstrap()
+moment = Moment()
+db = SQLAlchemy()
+mail = Mail()
+
+login_manage = LoginManager()
+login_manage.session_protection = 'strong'
+login_manage.login_view = 'auth.login'
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    bootstrap.init_app(app)
+    mail.init_app(app)
+    moment.init_app(app)
+    db.init_app(app)
+    login_manage.init_app(app)
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
+    return app
